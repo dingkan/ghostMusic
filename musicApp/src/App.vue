6811e65469e248465a581cd87fdc9d6342a51d7f
hcsx <657483726@qq.com>
2018-06-20 11:23:38 +0800

    *./ 项目初始化。目录结构。配置文件

---
+++ musicApp/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang='stylus' rel='stylesheet/stylus'>
+#app
+  color color-background
+</style>
